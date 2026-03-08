4b7129ae94edb1d24dd7f65727500960df1c4a03
VladimirGalstyan <vladimir.galstyan030@gmail.com>
2026-03-09 00:56:21 +0300

    Add files via upload

---
+++ Dish.java
@@ -0,0 +1,21 @@
+package ArrayListExercise.First;
+
+class Dish {
+    private String name;
+    private int price;
+
+    public Dish(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public int getPrice() {
+        return price;
+    }
+
+}
